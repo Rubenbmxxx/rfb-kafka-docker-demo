5209df137b3812f2d06919308a957d702e317010
rubenferrer <rubenferrer>
2024-01-09 17:44:40 +0100

    store data into postgres database for future analysis

--- consumer/kafka_consumer.py
+++ consumer/kafka_consumer.py
@@ -4,24 +4,25 @@ import json
 # from datetime import datetime
 import psycopg2
 
-# def store_pokemon(pokemon, level, health, attack, defense):
-#     sql = """
-#     INSERT INTO pokedex.random_appears(pokemon, level, health, attack, defense)
-#     VALUES ( %s, %s, %s, %s, %s );
-#     """
-#
-#     cur.execute(sql, (pokemon, level, health, attack, defense))
-#     conn.commit()
-#
-#
-# conn = psycopg2.connect(
-#     database="postgresdb",
-#     host="localhost",
-#     user="postgres",
-#     password="postgres",
-#     port="5432"
-# )
-# cur = conn.cursor()
+
+def store_pokemon(pokemon, level, health, attack, defense):
+    sql = """
+    INSERT INTO pokedex.random_appears(pokemon, level, health, attack, defense)
+    VALUES ( %s, %s, %s, %s, %s );
+    """
+
+    cur.execute(sql, (pokemon, level, health, attack, defense))
+    conn.commit()
+
+
+conn = psycopg2.connect(
+    database="postgresdb",
+    host="postgres",
+    user="postgres",
+    password="postgres",
+    port="5432"
+)
+cur = conn.cursor()
 
 consumer = Consumer({
     'bootstrap.servers': 'kafka',
@@ -46,7 +47,12 @@ while True:
 
     # print('Received message: {}'.format(message.value().decode('utf-8')))
     print('Received message: {}'.format(message.value()))
-    print('Pokemon: ' + data.get('pokemon'))
+    # print('Pokemon: ' + data.get('pokemon'))
+    store_pokemon(pokemon=data.get('pokemon'),
+                  level=data.get('level'),
+                  health=data.get('health'),
+                  attack=data.get('attack'),
+                  defense=data.get('defense'))
 
     time.sleep(5)
 
--- producer/data_streaming.py
+++ producer/data_streaming.py
@@ -6,8 +6,14 @@ import time
 # Kafka Topic
 topic_name = 'pokemon'
 
+
 # Test Pokemon names
-pokemons = ["Bulbasaur", "Charmander", "Squirtle", "Pikachu", "Jigglypuff", "Snorlax", "Mewtwo", "Eevee"]
+# pokemons = ["Bulbasaur", "Charmander", "Squirtle", "Pikachu", "Jigglypuff", "Snorlax", "Mewtwo", "Eevee"]
+
+def read_file(path):
+    with open(path, 'r') as f:
+        data = json.load(f)
+    return data
 
 
 def delivery_report(err, msg):
@@ -20,10 +26,13 @@ def delivery_report(err, msg):
 # Start kafka producer
 producer = Producer({'bootstrap.servers': 'kafka'})
 
+
+pokedex_data = read_file(path='pokedex.json')
+
 # Send random pokemon data to pokemon topic
 while True:
     pokemon_data = {
-        'pokemon': random.choice(pokemons),
+        'pokemon': random.choice(pokedex_data),
         'level': random.randint(1, 100),
         'health': random.randint(50, 100),
         'attack': random.randint(20, 50),
